b449490f51dccaa4c229fa6ea1bb399a5096f2ba
River Dillon <river@k-monk.org>
2024-12-01 21:49:25 -0500

    add rough file reading interface

---
+++ include/file.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * file.h - storage interface
+ *
+ * I'm not worrying about making this a complete interface, for now.
+ ******************************************************************************/
+
+#pragma once
+
+#include "prelude.h"
+
+Byte* platform_read_file(const Char* path, Index* size);
+Void platform_free_file(Byte* content);
---
+++ src/windows/file.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "windows/wrapper.h"
+#include "file.h"
+#include "log.h"
+
+Byte* platform_read_file(const Char* path, Index* size)
+{
+  // open file for reading
+  const HANDLE handle = CreateFile(
+      path,                   // file to open
+      GENERIC_READ,           // open for reading
+      FILE_SHARE_READ,        // share for reading
+      NULL,                   // default security
+      OPEN_EXISTING,          // existing file only
+      FILE_ATTRIBUTE_NORMAL,  // normal file
+      NULL);                  // no attr. template
+  if (handle == INVALID_HANDLE_VALUE) {
+    return NULL;
+  }
+
+  // get file size
+  LARGE_INTEGER file_size;
+  const BOOL file_size_status = GetFileSizeEx(handle, &file_size);
+  ASSERT(file_size.HighPart == 0);
+  if (file_size_status == FALSE) {
+    return NULL;
+  }
+
+  // allocate memory
+  Byte* const buffer = malloc(file_size.LowPart + 1);
+  ASSERT(buffer);
+
+  // read file
+  DWORD bytes_read = 0;
+  const BOOL read_file_status = ReadFile(
+      handle,                 // file handle
+      buffer,                 // out content
+      file_size.LowPart,      // number of bytes to read
+      &bytes_read,            // number of bytes read
+      NULL);                  // overlapped
+  ASSERT(bytes_read == file_size.LowPart);
+  if (read_file_status == FALSE) {
+    return NULL;
+  }
+
+  // insert null terminator
+  buffer[file_size.LowPart] = 0;
+
+  // write out file size and return
+  *size = (Index) bytes_read;
+  return buffer;
+}
+
+Void platform_free_file(Byte* content)
+{
+  free(content);
+}
